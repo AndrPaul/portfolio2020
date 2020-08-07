fac249da019d77e7e6350aaade523efb5a847c69
Paul Andrica <andpau96@gmail.com>
2020-08-07 09:04:17 +0000

    server changes

--- server/server.js
+++ server/server.js
@@ -10,31 +10,16 @@ const helmet = require("helmet");
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/../client/build')));
-var allowedOrigins = [
-  'http://161.35.158.203:3000',
-  'http://localhost:3000',
-  'http://localhost:5000',
-  'http://161.35.158.203'
-];
 
 
-app.use(cors({
-  origin: function (origin, callback) {    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true); if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    } return callback(null, true);
-  }
-}));
+app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", routeApi);
 app.use('/sendme', routeSendMe);
-app.get('*', (req,res) =>{
+app.get('*', (req,res,next) =>{
   res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 });
 
