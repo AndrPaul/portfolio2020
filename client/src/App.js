2066080c0211ecb53864e393316ce82f95e11b8c
Paul Andrica <andpau96@gmail.com>
2020-08-16 15:01:13 +0200

    added cookies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./components/home/Home";
@@ -10,13 +10,13 @@ import Footer from "./components/footer/Footer";
 import axios from 'axios';
 import NotFound from "./components/404/NotFound"
 import ScrollToTop from './ScrollToTop';
-
+import ReactGA from 'react-ga';
 import "./sass/App.scss";
 
 
 
 const App = (props) => {
- 
+
   const [header, setHeader] = useState({});
 
   useEffect(() => {
@@ -25,29 +25,34 @@ const App = (props) => {
 
         setHeader(res.data)
         // return[...res.data]
-      })
+      });
+
+    ReactGA.initialize('UA-175516835-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
   }, []);
-  
- 
+
+
   return (
 
     <Router>
- 
+
       <div className="App">
-      <ScrollToTop />
+        <ScrollToTop />
         <NavBar />
 
         <main className="app-wrap">
           <AnimatePresence exitBeforeEnter>
-          <Switch>
-            <Route path="/" exact render={Home} />
-            <Route path="/portfolio"  render={() => (<Portfolio title={header.length > 0 ? header[0].portfolio.title : ''} subTitle={header.length > 0 ? header[0].portfolio.subTitle : ''} />)} />
-            <Route path="/cv" render={() => (<CV title={header.length > 0 ? header[0].cv.title : ''} subTitle={header.length > 0 ? header[0].cv.subTitle : ''} />)} />
-            <Route path="/contact"  render={() => (<Contact title={header.length > 0 ? header[0].contact.title : ''} subTitle={header.length > 0 ? header[0].contact.subTitle : ''} />)} />
-            <Route path="" component={NotFound} />
-          </Switch>
+            <Switch>
+              <Route path="/" exact render={Home} />
+              <Route path="/portfolio" render={() => (<Portfolio title={header.length > 0 ? header[0].portfolio.title : ''} subTitle={header.length > 0 ? header[0].portfolio.subTitle : ''} />)} />
+              <Route path="/cv" render={() => (<CV title={header.length > 0 ? header[0].cv.title : ''} subTitle={header.length > 0 ? header[0].cv.subTitle : ''} />)} />
+              <Route path="/contact" render={() => (<Contact title={header.length > 0 ? header[0].contact.title : ''} subTitle={header.length > 0 ? header[0].contact.subTitle : ''} />)} />
+              <Route path="" component={NotFound} />
+            </Switch>
           </AnimatePresence>
         </main>
+
         <Footer />
       </div>
     </Router>
